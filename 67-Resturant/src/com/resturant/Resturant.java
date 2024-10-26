9182cde51346582dfc08f501b8780399ce40d36c
antaryami <papu@gmail.com>
2024-10-26 20:38:39 +0530

    Resturant || changes

---
+++ 67-Resturant/src/com/resturant/Resturant.java
@@ -0,0 +1,18 @@
+package com.resturant;
+
+import java.util.HashMap;
+
+public class Resturant {
+
+	public static void main(String[] args) {
+		HashMap<String, Integer> iteams= new HashMap<String, Integer>();
+		iteams.put("Rice", 10);
+		iteams.put("Dal", 20);
+		iteams.put("Chicken Curry", 80);
+		iteams.put("Salad", 10);
+		iteams.put("Papad", 10);
+		System.out.println(iteams);
+		
+	}
+
+}
